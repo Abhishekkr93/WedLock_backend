688310d8d9d7dc2a2f3d841021b70a72d91e2ccc
vimalverma558 <kumarvimal558@gmail.com>
2021-04-11 07:35:00 +0530

    redirect url change 🚀

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./model/User')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 // mongodb setup
@@ -12,7 +13,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 // server and static file setup
 const app= express();
 const port = process.env.PORT || 5000
-app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/',(req,res)=>{
   res.send("hello boy")
@@ -26,7 +27,7 @@ app.post('/',(req,res)=>{
   })
   user.save()
   .then(newuser =>{
-    res.send(newuser)
+    res.redirect('http://wedlock.netlify.app/thanks.html')
   })
   .catch(err => res.send(err))
 })
